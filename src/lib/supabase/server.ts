dae89ec1eba91e32e17e15e5e2233fe5377a0077
tony3sord <tressordtony@gmail.com>
2026-06-19 18:33:10 -0400

    add catch error in supabase server config

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -17,9 +17,13 @@ export async function createClient() {
             options?: Record<string, unknown>;
           }[],
         ) => {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, options),
-          );
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch {
+            console.log("error in cookies");
+          }
         },
       },
     },
